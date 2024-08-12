6096c15d0086cfd84c5d4564b88ac9ae25133476
far1h <farihmuhammad45@gmail.com>
2024-08-12 21:51:22 +0700

    create coverimageview, add JSON data model for cover images, decode JSON data to page tabview, add coverimageview to contentview

---
+++ IndoWilderness/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  IndoWilderness
+//
+//  Created by Farih Muhammad on 12/08/2024.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
---
+++ IndoWilderness/View/CoverImageView.swift
@@ -0,0 +1,36 @@
+//
+//  CoverImageView.swift
+//  IndoWilderness
+//
+//  Created by Farih Muhammad on 12/08/2024.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    // MARK: - PROPERTIES
+    
+    let coverImages: [CoverImage] = Bundle.main.decode("covers.json")
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        TabView {
+            ForEach(coverImages) { item in
+                Image(item.name)
+                    .resizable()
+                .scaledToFill()
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+// MARK: - PREVIEW
+
+struct CoverImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
+    }
+}
